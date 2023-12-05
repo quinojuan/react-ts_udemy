7d383243107a309eb7acb93a957c05a7fbbd4f44
quinojuan <quinojuan@gmail.com>
2023-12-05 18:03:16 -0300

    Crear una pequeña paginación

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,20 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { reqResApi } from "../api/reqRes";
 import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
-
-const [usuarios, setUsuarios] = useState<Usuario[]>([])
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
   useEffect(() => {
     // llamado a la API
     reqResApi
       .get<ReqResListado>("/users")
       .then((res) => {
-        console.log(res.data.data);
+        setUsuarios(res.data.data);
       })
       .catch(console.log);
   }, []);
+
+  const renderItem = ({
+    avatar,
+    email,
+    first_name,
+    last_name,
+    id,
+  }: Usuario) => {
+    return (
+      <tr key={id.toString()}>
+        <td><img src={ avatar } alt={last_name} style={{
+          width: 40,
+          borderRadius: 50
+        }} /></td>
+        <td>
+          {first_name} {last_name}
+        </td>
+        <td>{email}</td>
+      </tr>
+    );
+  };
 
   return (
     <>
@@ -27,8 +47,13 @@ const [usuarios, setUsuarios] = useState<Usuario[]>([])
             <th>email</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {
+            usuarios.map(renderItem) // tambien podria ser usuarios.map( usuario => renderItem (usuario))
+          }
+        </tbody>
       </table>
+      <button className="btn btn-primary">Siguientes</button>
     </>
   );
 };
