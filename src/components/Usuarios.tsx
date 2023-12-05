6858790a2d85104e6c97e0db2b77f5a44d735fb8
quinojuan <quinojuan@gmail.com>
2023-12-05 18:27:08 -0300

    Custom Hook - useUsuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,19 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { reqResApi } from "../api/reqRes";
 import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const paginaRef = useRef(0);
 
   useEffect(() => {
     // llamado a la API
-    reqResApi
-      .get<ReqResListado>("/users")
-      .then((res) => {
-        setUsuarios(res.data.data);
-      })
-      .catch(console.log);
+    cargarUsuarios();
   }, []);
+
+  const cargarUsuarios = async () => {
+    const res = await reqResApi.get<ReqResListado>("/users", {
+      params: {
+        page: paginaRef.current,
+      },
+    });
+    if (res.data.data.length > 0) {
+      setUsuarios(res.data.data);
+      console.log(res.data.data);
+      paginaRef.current++;
+    } else {
+      alert("No hay mas registros");
+    }
+  };
 
   const renderItem = ({
     avatar,
@@ -24,10 +35,16 @@ export const Usuarios = () => {
   }: Usuario) => {
     return (
       <tr key={id.toString()}>
-        <td><img src={ avatar } alt={last_name} style={{
-          width: 40,
-          borderRadius: 50
-        }} /></td>
+        <td>
+          <img
+            src={avatar}
+            alt={last_name}
+            style={{
+              width: 40,
+              borderRadius: 50,
+            }}
+          />
+        </td>
         <td>
           {first_name} {last_name}
         </td>
@@ -53,7 +70,9 @@ export const Usuarios = () => {
           }
         </tbody>
       </table>
-      <button className="btn btn-primary">Siguientes</button>
+      <button className="btn btn-primary" onClick={() => cargarUsuarios()}>
+        Siguientes
+      </button>
     </>
   );
 };
