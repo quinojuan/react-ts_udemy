0b22c206480c123e973d5cd10c94f513b5657156
quinojuan <quinojuan@gmail.com>
2023-12-05 23:38:22 -0300

    formularios en curso

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export const Formularios = () => {
+  const [formulario, setFormulario] = useState({
+    email: "test@test.com",
+    password: "123456",
+  });
+
+  const onChange = (value: string, campo: string) => {
+    setFormulario({
+      ...formulario,
+      [campo]: value,
+    });
+  };
+
   return (
     <>
       <h3>Formularios</h3>
-      <input type="text" className="form-control" />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Email"
+        value={formulario.email}
+      />
+
+      <input
+        type="text"
+        className="form-control mb-2 mt-2"
+        placeholder="Password"
+        value={formulario.password}
+      />
     </>
   );
 };
